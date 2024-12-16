e5ce9072a3b02635ca6e729671f59df74c2d8e25
ahmadabbani <ahmdabbanyy@gmail.com>
2024-12-16 21:25:14 +0200

    .

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,23 @@
 // config/plugins.js
-module.exports = {
+module.exports = ({ env }) => ({
+  // ...
   jwt: {
     expiresIn: "7d", // Set token expiration to 7 days or more
   },
-};
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
